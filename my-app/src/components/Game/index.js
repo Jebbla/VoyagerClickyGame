ec1c3a9209454486cd88ac740de5357aef9c7b86
Jebbla <jlp2722@gmail.com>
2019-09-14 10:36:26 -0500

    Set files

---
+++ my-app/src/components/Game/index.js
@@ -0,0 +1,10 @@
+import Jumbotron from "../Jumbotron";
+import Header from "../Header";
+import React, { Component } from 'react';
+import data from "../../Data";
+import Cards from "../Cards";
+
+
+import "./style.css";
+
+export default class index extends Component{}
