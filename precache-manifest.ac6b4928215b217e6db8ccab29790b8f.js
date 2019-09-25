89ca40bef3aa5ef8da7ab0f028a16bb41b0b418b
Jebbla <jlp2722@gmail.com>
2019-09-24 21:18:10 -0500

    Updates

---
+++ precache-manifest.ac6b4928215b217e6db8ccab29790b8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b578522d5b461cdd5e7496e3730a41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1578090c84a7bbd10ee",
+    "url": "/static/css/main.88a849c5.chunk.css"
+  },
+  {
+    "revision": "ecbea1bd3fcf2f13a66b",
+    "url": "/static/js/2.40713410.chunk.js"
+  },
+  {
+    "revision": "d1578090c84a7bbd10ee",
+    "url": "/static/js/main.18840ab0.chunk.js"
+  },
+  {
+    "revision": "cb4fd463e7184abd8f6a",
+    "url": "/static/js/runtime~main.d52357dd.js"
+  }
+]);
